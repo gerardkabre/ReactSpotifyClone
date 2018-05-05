73e1a746585c302d9616b78f32a1ee9bd3270679
Gerard Cabrerizo <gerard.kabre@gmail.com>
2018-05-05 21:32:06 +0200

    login component change

--- spotify-clone/src/Pages/Login.js
+++ spotify-clone/src/Pages/Login.js
@@ -11,7 +11,6 @@ const authUrl =
   'https://accounts.spotify.com/authorize?client_id=7601320fe8e34a45b95e142c37e48b52&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
 
 class Login extends Component {
-
   componentDidMount() {
     if (!this.props.isLoggedIn) {
       if (!this.props.token) return (window.location.href = authUrl);
@@ -19,11 +18,7 @@ class Login extends Component {
   }
 
   render() {
-    return (
-      <Container>
-        <TitleSubtitle title="The jazzy Page" subtitle="Discover new Jazz artists, bands and playlists" />
-      </Container>
-    );
+    return null;
   }
 }
 
