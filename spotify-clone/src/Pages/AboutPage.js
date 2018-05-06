2865a4058d98c504953dc5e447217ff148a3cbed
Gerard Cabrerizo <gerard.kabre@gmail.com>
2018-05-06 14:28:37 +0200

    about page text

--- spotify-clone/src/Pages/AboutPage.js
+++ spotify-clone/src/Pages/AboutPage.js
@@ -6,9 +6,15 @@ import TitleSubtitle from '../components/Title&Subtitle/TitleSubtitle';
 const AboutPage = () => (
   <Container>
     <TitleSubtitle title="About Page" subtitle="What is this site about?" />
-    What this is
+    <div style={{color: 'grey'}}>
+    <p> This is an open source app showcasing a spotify 'clone' with basic functionality, 
+      uses your spotify user and retrives your recently palyed songs, albums, all the songs
+      you have saved, and allows you to play a sample of them form your browser.
+    </p>
+    <p> Built with React, Redux, Thunk, React-Router and React-bootstrap</p>
+    <p> You can see the code here: <a style={{color: '#1DB954'}} href="https://github.com/gerardkabre/ReactSpotifyClone">Github</a></p>
+    </div>
   </Container>
 );
 
 export default AboutPage;
-
